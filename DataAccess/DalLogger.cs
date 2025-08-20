5c3d333d564ea723790880f2a1bbf0bf3f344a3a
Maher Al-Dobal <maher1999git@gamil.com>
2025-08-20 18:54:28 +0300

    feat(DAL): Add 'DalLogger' class for simple console logging
    This is a simple console logger implementation.
    File logging will be added later.

---
+++ DataAccess/DalLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+/// <summary>
+/// Logger for Data Access Layer (DAL) operations.
+/// </summary>
+public static class DalLogger
+{
+    /// <summary>
+    /// It Logs exceptions and their additional information to console.
+    /// You can add more information to the log message by passing additionalInfo parameter.
+    /// If you want to log non-exception cases, you can pass null for the exception parameter.
+    /// </summary>
+    /// <param name="ex"></param>
+    /// <param name="additionalInfo"></param>
+    /// <param name="memberName"></param>
+    /// <param name="filePath"></param>
+    /// <param name="lineNumber"></param>
+    public static void Log(
+        Exception ex = null,
+        string additionalInfo = "",
+        [CallerMemberName] string memberName = "",
+        [CallerFilePath] string filePath = "",
+        [CallerLineNumber] int lineNumber = 0)
+    {
+        string className = System.IO.Path.GetFileNameWithoutExtension(filePath);
+
+        string logMessage = $"||{DateTime.Now:yyyy-MM-dd HH:mm:ss} " +
+                            $"||Class: {className}, " +
+                            $"||Method: {memberName}, " +
+                            $"||Line: {lineNumber}, " +
+                            $"||Exception: {ex?.ToString() ?? "No exception"}, " +
+                            $"||Additional Info: {additionalInfo}";
+
+        // for now we log to console : (will log to file later on)
+        Console.WriteLine(logMessage);
+    }
+}
