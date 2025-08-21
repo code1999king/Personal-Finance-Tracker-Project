76d5911202f89e28dda37ea87bc806ab62c70997
Maher Al-Dobal <maher1999git@gamil.com>
2025-08-21 07:16:09 +0300

    feat(User): Add basic implementation of 'User' class in BLL as domain model
    Add basic properties and constructors

---
+++ BusinessLogic/Users/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace BusinessLogic.Users
+{
+    /// <summary>
+    /// Represents user's domain model.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// User's identifier.
+        /// </summary>
+        public int UserID { get; set; }
+
+        /// <summary>
+        /// User's username.
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// User's registration date.
+        /// </summary>
+        public DateTime RegisteredAt { get; set; }
+
+        /// <summary>
+        /// User's current balance.
+        /// </summary>
+        public decimal CurrentBalance { get; set; }
+
+        /// <summary>
+        /// Creates empty user object, allows you to fill it later.
+        /// </summary>
+        public User()
+        {
+            UserID = -1;
+            Username = "";
+            RegisteredAt = DateTime.Now;
+            CurrentBalance = 0;
+        }
+
+        /// <summary>
+        /// Creates user object with predefined values. Private access modifier prevents creating a full user object from outside this class.
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="username"></param>
+        /// <param name="registeredAt"></param>
+        /// <param name="currentBalance"></param>
+        private User(int userID, string username, DateTime registeredAt, decimal currentBalance)
+        {
+            UserID = userID;
+            Username = username;
+            RegisteredAt = registeredAt;
+            CurrentBalance = currentBalance;
+        }
+    }
+}
